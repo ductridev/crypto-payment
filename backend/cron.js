d588f6cabe51635492e88417aef739e4b05703a3
TrisKnightGamer <trihd123@gmail.com>
2022-07-16 00:47:33 +0700

    remove gsn relayer

--- backend/cron.js
+++ backend/cron.js
@@ -1,21 +1,15 @@
 var cron = require('node-cron');
+const logger = require('./utils/logger');
 const https = require('https');
 const Web3 = require('web3');
 const { RelayProvider } = require('@opengsn/provider');
 const ObjectId = require('mongodb').ObjectId; 
 
+const {Biconomy} = require('@biconomy/mexa');
+
 var mongoDB = require('./db');
-const paymasterAddress = require('../build/gsn/Paymaster.json').address;
 
-const config = { 
-    paymasterAddress,
-    loggerConfiguration: {
-        logLevel: 'debug',
-        // loggerUrl: 'logger.opengsn.org',
-    }
-}
-
-const updateTokenPriceTask = cron.schedule("*/15 * * * *", async () => {
+const updateTokenPriceInUSDTask = cron.schedule("*/15 * * * *", async () => {
     const options = {
         "method": "GET",
         "hostname": "rest.coinapi.io",
@@ -35,7 +29,6 @@ const updateTokenPriceTask = cron.schedule("*/15 * * * *", async () => {
         response.on('end', function () {
             try {
                 chunks = JSON.parse(Buffer.concat(chunks).toString());
-                console.log(chunks);
                 const db = client.db(dbName);
                 var collection = db.collection(collectionName);
                 chunks.rates.forEach((element) => {
@@ -45,27 +38,27 @@ const updateTokenPriceTask = cron.schedule("*/15 * * * *", async () => {
 
                         if (queryCollectionErr) {
 
-                            console.log(`Unable to query document(s) on the collection "${collectionName}". Error: ${queryCollectionErr}`);
+                            logger.error(`Unable to query document(s) on the collection "${collectionName}". Error: ${queryCollectionErr}`);
 
                         } else if (result.length) {
 
                             collection.updateOne({ assetIdQuote: element.asset_id_quote }, { $set: { lastUpdatedTime: element.time, rate: element.rate } }).then((obj) => {
                                 if (obj) {
-                                    console.log('Updated - ', obj);
+                                    logger.log('Updated - ', obj);
                                 } else {
-                                    console.log(`No document found with defined "${element.asset_id_quote}" criteria!`);
+                                    logger.log(`No document found with defined "${element.asset_id_quote}" criteria!`);
                                 }
                             }).catch((e) => {
-                                console.log(`Unable to update document to the collection "${collectionName}". Error: ${e}`);
+                                logger.error(`Unable to update document to the collection "${collectionName}". Error: ${e}`);
                             });
 
                         } else {
 
                             collection.insertOne(input, (insertCollectionErr, result) => {
                                 if (insertCollectionErr) {
-                                    console.log(`Unable to insert document to the collection "${collectionName}". Error: ${insertCollectionErr}`);
+                                    logger.error(`Unable to insert document to the collection "${collectionName}". Error: ${insertCollectionErr}`);
                                 } else {
-                                    console.log(`Inserted ${result.length} documents into the "${collectionName}" collection. The documents inserted with "_id" are: ${result.insertedId}`);
+                                    logger.log(`Inserted ${result.length} documents into the "${collectionName}" collection. The documents inserted with "_id" are: ${result.insertedId}`);
                                 }
                             });
 
@@ -74,7 +67,7 @@ const updateTokenPriceTask = cron.schedule("*/15 * * * *", async () => {
                     });
                 });
             } catch (apiResponeErr) {
-                console.log(`Unable to get data from API. Error: ${apiResponeErr}`);
+                logger.error(`Unable to get data from API. Error: ${apiResponeErr}`);
             }
         });
     });
@@ -86,9 +79,14 @@ const updateTokenPriceTask = cron.schedule("*/15 * * * *", async () => {
 
 const sendBatchTransaction = cron.schedule("*/2 * * * *", async () => {
 
+    let biconomy = new Biconomy(new Web3.providers.HttpProvider(process.env.INFURA_API),{
+        // walletProvider: <Wallet Provider>, 
+        apiKey: process.env.ETH_BICONOMY_APIKEY, 
+        debug: false
+      }); 
+
     // const provider = await RelayProvider.newProvider({ provider: process.env.INFURA_API || EthereumWeb3.currentProvider, config }).init();
-    const provider = await RelayProvider.newProvider({ provider: new Web3.providers.HttpProvider('http://127.0.0.1:8545'), config }).init()
-    const EthereumWeb3 = new Web3(provider);
+    const EthereumWeb3 = new Web3(biconomy);
     
     var batch = new EthereumWeb3.BatchRequest();
 
@@ -113,7 +111,7 @@ const sendBatchTransaction = cron.schedule("*/2 * * * *", async () => {
 
         if (queryCollectionErr) {
 
-            console.log(`Unable to query document(s) on the collection "${collectionName}". Error: ${queryCollectionErr}`);
+            logger.error(`Unable to query document(s) on the collection "${collectionName}". Error: ${queryCollectionErr}`);
 
         } else if (result.length) {
             for (let i = 0; i < result.length; i++) {
@@ -121,9 +119,9 @@ const sendBatchTransaction = cron.schedule("*/2 * * * *", async () => {
                     EthereumWeb3.eth.sendSignedTransaction(result[i].rawTransaction).on('receipt', (_result) => {
                         collection1.insertOne({ receipt: _result.transactionHash, amount: result[i].amount, transaction_id: result[i]._id, status: _result.status ? 'success' : 'failed' }, (insertCollectionErr, __result) => {
                             if (insertCollectionErr) {
-                                console.log(`Unable to insert document to the collection "${collectionName}". Error: ${insertCollectionErr}`);
+                                logger.error(`Unable to insert document to the collection "${collectionName}". Error: ${insertCollectionErr}`);
                             } else {
-                                console.log(`Inserted ${__result.length} documents into the "${collectionName}" collection. The documents inserted with "_id" are: ${__result.insertedId}`);
+                                logger.log(`Inserted ${__result.length} documents into the "${collectionName}" collection. The documents inserted with "_id" are: ${__result.insertedId}`);
 
                                 // if (ws.readyState === 1) {
                                 //     ws.onmessage((msg) => {
@@ -140,9 +138,9 @@ const sendBatchTransaction = cron.schedule("*/2 * * * *", async () => {
                         })
                         collection.updateOne({_id: new ObjectId(result[i]._id)}, {$set: {status: _result.status ? 'success' : 'failed'}}).then((obj) => {
                             if (obj) {
-                                console.log('Updated - ', obj);
+                                logger.log('Updated - ', obj);
                             } else {
-                                console.log(`No document found with defined "${result[i]._id}" criteria!`);
+                                logger.log(`No document found with defined "${result[i]._id}" criteria!`);
                             }
                         });
                     })
@@ -162,5 +160,5 @@ const sendBatchTransaction = cron.schedule("*/2 * * * *", async () => {
 
 console.log('Cron started');
 
-// updateTokenPriceTask.start();
+// updateTokenPriceInUSDTask.start();
 sendBatchTransaction.start();
