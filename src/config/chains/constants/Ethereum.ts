1b4d759ddc0ae3575483a6107e5c11ef51e8d4ef
TrisKnightGamer <trihd123@gmail.com>
2022-08-07 18:47:00 +0700

    Big Update

---
+++ src/config/chains/constants/Ethereum.ts
@@ -0,0 +1,24 @@
+import { ChainConfig } from "..";
+import { NATIVE_ADDRESS } from "../../constants";
+import ethIcon from "../../assets/images/tokens/eth-icon.svg"
+
+export const ETHEREUM: ChainConfig = {
+    name: "Ethereum",
+    image: ethIcon,
+    subText: "Ethereum Mainnet",
+    chainId: 1,
+    rpcUrl: "https://eth-mainnet.alchemyapi.io/v2/wO7WAmNPAsZFhRlpd-xYjM-5Pl5Dx8-G",
+    currency: "ETH",
+    nativeToken: NATIVE_ADDRESS,
+    nativeDecimal: 18,
+    nativeFaucetURL: "",
+    assetSentTopicId:
+      "0xec1dcc5633614eade4a5730f51adc7444a5103a8477965a32f2e886f5b20f694",
+    biconomy: {
+      enable: false,
+      apiKey: "fWz3rAdDl.44d92a99-9ca4-47b1-98ca-aa2bae068e38",
+    },
+    graphURL: "https://api.thegraph.com/subgraphs/name/divyan73/hyphenethereumv2",
+    networkAgnosticTransfer: false,
+    explorerUrl: "https://etherscan.io/",
+  };
---
+++ src/config/constants.ts
@@ -0,0 +1,11 @@
+export const NATIVE_ADDRESS = "0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee";
+export const DEFAULT_FIXED_DECIMAL_POINT = 3;
+export const LP_FEE_FRACTION = 0.1;
+// export const MANUAL_EXIT_RETRIES = 12;
+
+const CONSTANTS = {
+  NATIVE_ADDRESS,
+  DEFAULT_FIXED_DECIMAL_POINT
+}
+
+export default CONSTANTS;
